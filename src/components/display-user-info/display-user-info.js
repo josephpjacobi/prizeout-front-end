15c179b4b0db90c01f5103fa3f24d4de39306b05
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-05-19 13:45:25 -0400

    added formatting for user info display

--- src/components/display-user-info/display-user-info.js
+++ src/components/display-user-info/display-user-info.js
@@ -1,17 +1,18 @@
 import { convertCentsToDollars } from "../../helper";
+import "./display-user-info.css";
 
 function DisplayUserInfo({ user }) {
   const { email, currency, balance } = user;
   return (
-    <div className="user-info-container">
-      <div>Email: {email}</div>
-      <div>Currency: {currency}</div>
-      <div>
-        Available Balance: {currency}
-        {convertCentsToDollars(balance)}
-      </div>
-    </div>
-  );
+		<div className="user-info-container">
+			<div className="info">Email: {email}</div>
+			<div className="info">Currency: {currency}</div>
+			<div className="info">
+				Available Balance: {currency}
+				{convertCentsToDollars(balance)}
+			</div>
+		</div>
+	);
 }
 
 export default DisplayUserInfo;
